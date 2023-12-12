5e9e3f9477d576340bf38e37af08c537331ae9f1
“quac88” <mac2@thrasher.com>
2023-12-12 16:30:12 -0600

    feat: update for user choice prompts and flask

--- model/main.py
+++ model/main.py
@@ -1,6 +1,9 @@
 import json
 from client import ScaleAIClient
 from utils import TokenizerWrapper
+from embeddings import SentenceEmbedder
+import torch
+import sys  
 
 # Configuration
 API_KEY = "live_a04ee4b972384b13bac7597080aface6"  # Replace with your actual API key
@@ -15,23 +18,31 @@ MODEL_TYPES = [
 def run_moe_system(input_text):
     client = ScaleAIClient(API_KEY)
     tokenizer_wrapper = TokenizerWrapper("gpt2", 2048)
+    sentence_embedder = SentenceEmbedder()  # Initialize your sentence embedder
 
     responses = []
+    embeddings = []
     for model_type in MODEL_TYPES:
         response = client.forward(model_type, input_text)
         responses.append(response)
 
-    best_response = select_best_response(responses)
+        # Generate embedding for each response
+        embedding = sentence_embedder([response.get('text', '')])
+        embeddings.append(embedding)
+
+    best_response = select_best_response(responses, embeddings)
     return best_response
 
-def select_best_response(responses):
-    # Implement your logic to select the best response
-    # This is a placeholder for your selection criteria
-    # Example: Selecting the longest response
-    best = max(responses, key=lambda resp: len(resp.get('text', '')))
-    return best.get('text', '')
+def select_best_response(responses, embeddings):
+    # Selecting the response with the embedding closest to the average
+    avg_embedding = sum(embeddings) / len(embeddings)
+    best_idx = min(range(len(embeddings)), key=lambda i: torch.norm(embeddings[i] - avg_embedding))
+    return responses[best_idx].get('text', '')
 
 if __name__ == "__main__":
-    input_text = "Tell me fun facts about ants"  # Replace with the text you want to process
-    best_answer = run_moe_system(input_text)
-    print("Best Response:", best_answer)
+    if len(sys.argv) > 1:
+        prompt = " ".join(sys.argv[1:])  # Combine all command-line arguments into a single string
+        best_answer = run_moe_system(prompt)
+        print("Best Response:", best_answer)
+    else:
+        print("Usage: python script_name.py 'your prompt here'")
--- model/server.py
+++ model/server.py
@@ -1,48 +1,19 @@
-import requests
 from flask import Flask, request, jsonify
-from typing import NamedTuple, Dict
+from main import run_moe_system
 
 app = Flask(__name__)
 
-# API configuration
-SCALE_AI_ENDPOINT = "https://api.donovan.scale.com/v1/chat"
-SCALE_AI_API_KEY = "live_a04ee4b972384b13bac7597080aface6"  # Replace with your actual API key
-
-class MakotoResponse(NamedTuple):
-    response_text: str
-
-
-def query_model_api(model_type, text):
-    payload = {
-        "modelType": model_type,
-        "text": text,
-        "workspace": "atrjw4p7vy9zi9p7kov0a40v",
-        "thread": "thread"
-    }
-    headers = {
-        "accept": "application/json",
-        "content-type": "application/json",
-        "authorization": f"Bearer {SCALE_AI_API_KEY}"
-    }
-    response = requests.post(SCALE_AI_ENDPOINT, json=payload, headers=headers)
-    return response.json()
-
-
-@app.route("/forward", methods=["POST"])
-def forward():
-    input_data = request.json
-    text = input_data["text"]
-
-    # Querying both models
-    response_gpt35 = query_model_api('OPENAI_GPT3.5-TURBO', text)
-    response_gpt4 = query_model_api('OPENAI_GPT4', text)
-
-    # Aggregation logic (this is a placeholder, modify as needed)
-    # For simplicity, we concatenate the responses from both models
-    aggregated_response = response_gpt35['response'] + " " + response_gpt4['response']
-
-    return jsonify(MakotoResponse(response_text=aggregated_response))
-
+@app.route("/query", methods=["POST"])
+def query():
+    input_data = request.get_json()
+    prompt = input_data.get("prompt", "")
+    
+    if not prompt:
+        return jsonify({"error": "Prompt is required"}), 400
+    
+    best_answer = run_moe_system(prompt)
+    
+    return jsonify({"response": best_answer})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=3000)
